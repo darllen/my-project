0566f7c9bb6cbaf4ca6273e483052a33e1b2a143
Aluno <aluno@ifpe>
2023-09-15 10:53:27 -0300

    a

--- screens/cadastroContatoScreen.js
+++ screens/cadastroContatoScreen.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { Text, Image, View, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, Image, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
-const CadastroContatoScreen = ({ navigation }) => {
+
+const CadastroContatoScreen = ({ route, navigation }) => {
+
+    const [getNome, setNome] = useState();
+    const [getEmail, setEmail] = useState();
+    const [getTelefone, setTelefone] = useState();
+
+
+    useEffect(() => {
+        if (route.params) {
+            setNome(route.params.nome);
+            setEmail(route.params.email);
+            setTelefone(route.params.telefone)
+        }
+    }, [])
+
+
+    function cadastrarContato() {
+        axios.post('http://localhost:3000/contatos', {
+            nome: getNome,
+            email: getEmail,
+            telefone: getTelefone
+        }).then(function (response) {
+            navigation.navigate('ListaContatos')
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+
     return (
         <View style={{ flex: 1, flexDirection: "colum", backgroundColor: '#ffffff' }}>
-            
+
             <View style={{ flex: 1, flexDirection: "row", backgroundColor: '#97D5B1', alignItems: "center", justifyContent: 'center' }}>
                 <TouchableOpacity onPress={() => navigation.navigate('ListaContatos')}>
                     <Image style={{ width: 30, height: 30 }}
@@ -15,11 +45,13 @@ const CadastroContatoScreen = ({ navigation }) => {
                 <Text style={{ fontWeight: 'bold', fontSize: 30, color: 'white', marginRight: 35, paddingLeft: 40 }}>Contato</Text>
             </View>
 
-            
+
             <View style={{ flex: 8, alignItems: 'center', justifyContent: 'center' }}>
 
                 <Text style={{ fontWeight: 'bold', fontSize: 15, width: '75%', marginBottom: '2%' }}>Nome </Text>
                 <TextInput
+                    onChangeText={nome => { setNome(nome) }}
+                    defaultValue={getNome}
                     style={{
                         height: 45,
                         width: '75%',
@@ -31,6 +63,8 @@ const CadastroContatoScreen = ({ navigation }) => {
                 />
                 <Text style={{ fontWeight: 'bold', fontSize: 15, marginTop: '7%', width: '75%', marginBottom: '2%' }}>Email </Text>
                 <TextInput
+                    onChangeText={email => { setEmail(email) }}
+                    defaultValue={getEmail}
                     style={{
                         height: 45,
                         width: '75%',
@@ -42,6 +76,8 @@ const CadastroContatoScreen = ({ navigation }) => {
                 />
                 <Text style={{ fontWeight: 'bold', fontSize: 15, marginTop: '7%', width: '75%', marginBottom: '2%' }}>Telefone </Text>
                 <TextInput
+                    onChangeText={telefone => { setTelefone(telefone) }}
+                    defaultValue={getTelefone}
                     style={{
                         height: 45,
                         width: '75%',
@@ -55,9 +91,17 @@ const CadastroContatoScreen = ({ navigation }) => {
                 <View style={[{ width: "50%", margin: 5 }]}>
                     <Button
                         title="Salvar"
-                        onPress={() => Alert.alert('Verifique seu e-mail!')}
+                        onPress={() => { cadastrarContato() }
+                        }
                         color='#97D5B1'
                     />
+                    {
+                        
+
+
+                    }
+                    
+                    
                 </View>
 
             </View>
--- screens/listaContatosScreen.js
+++ screens/listaContatosScreen.js
@@ -2,30 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { Text, Image, View, TouchableOpacity } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
 import axios from 'axios';
+import { useIsFocused } from '@react-navigation/core';
 
 
 const ListaContatosScreen = ({ navigation }) => {
 
     const [dados, setDados] = useState([]);
 
+    const isFocused = useIsFocused()
 
     useEffect(() => {
-        consultarDados();
+        consultarContatos();
+    }, [isFocused])
 
-        function consultarDados() {
-            axios.get('http://localhost:3000/contatos')
-                .then(function (response) {
-                    setDados(response.data);
-                }).catch(function (error) {
-                    console.log(error);
-                });
-        }
-    }, [])
+    function consultarContatos() {
+        axios.get('http://localhost:3000/contatos')
+            .then(function (response) {
+                setDados(response.data);
+                console.log(dados)
+            }).catch(function (error) {
+                console.log(error);
+            });
+    }
 
 
     return (
         <View style={{ flex: 1, flexDirection: "colum" }}>
-
 
             <View style={{ flex: 1, flexDirection: "row", backgroundColor: '#97D5B1', alignItems: "center", justifyContent: 'center' }}>
                 <Text style={{ fontWeight: 'bold', fontSize: 25, color: 'white', marginRight: 35, paddingLeft: 40 }}>Lista de Contatos</Text>
@@ -38,29 +40,25 @@ const ListaContatosScreen = ({ navigation }) => {
                         style={{ width: 30, height: 30 }}
                     />
                 </TouchableOpacity>
-
             </View>
 
 
             <View style={{ flex: 8 }}>
-
                 {
                     dados.map((l, i) => (
-                        <ListItem key={i} bottomDivider onPress={() => navigation.navigate('AlterarContato', {
+                        <ListItem key={i} bottomDivider onPress={() => navigation.navigate('CadastroContato', {
                             nome: l.nome,
                             telefone: l.telefone,
                             email: l.email
-
                         })
                         }>
                             <Avatar rounded title={l.nome[0]} containerStyle={{ backgroundColor: "#c2c2c2" }} />
                             <ListItem.Content>
-                                
                                 <ListItem.Title>{l.nome}</ListItem.Title>
                                 <ListItem.Subtitle>{l.telefone}</ListItem.Subtitle>
                             </ListItem.Content >
                         </ListItem>
-                                              
+
                     ))
                 }
             </View>
